94a59edcd9c02c56049acb25ad6a6f8313b05768
juancarlosfermin <juancarlosfermin80@gmail.com>
2026-06-04 16:41:41 -0400

    Implement SymbolTable añadir

---
+++ AnalizadorSintactico/src/SymbolTable.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SymbolTable {
+    private Map<String, Symbol> table;
+
+    public SymbolTable() {
+        this.table = new HashMap<>();
+    }
+
+    public void addSymbol(String name, String type) throws SemanticException {
+        if (table.containsKey(name)) {
+            throw new SemanticException("Error semantico: El simbolo '" + name + "' ya esta declarado.");
+        }
+        table.put(name, new Symbol(name, type));
+    }
+
+    public Symbol getSymbol(String name) throws SemanticException {
+        if (!table.containsKey(name)) {
+            throw new SemanticException("Error semantico: Variable '" + name + "' no declarada.");
+        }
+        return table.get(name);
+    }
+}
