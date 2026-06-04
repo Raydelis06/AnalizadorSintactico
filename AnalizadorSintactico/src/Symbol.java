dc6b1d138840962afe4e93641a623f4b2e43defc
juancarlosfermin <juancarlosfermin80@gmail.com>
2026-06-04 16:41:19 -0400

    Create Symbol class

---
+++ AnalizadorSintactico/src/Symbol.java
@@ -0,0 +1,17 @@
+public class Symbol {
+    private String name;
+    private String type;
+
+    public Symbol(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
