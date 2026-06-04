96a2250dc9851e69c82ab7846be7ed4570fb6b26
juancarlosfermin <juancarlosfermin80@gmail.com>
2026-06-04 16:40:06 -0400

    Implement SemanticAnalyzer class for variable checks
    Implement SemanticAnalyzer class for variable checks

---
+++ AnalizadorSintactico/src/SemanticAnalyzer.java
@@ -0,0 +1,41 @@
+public class SemanticAnalyzer {
+    private SymbolTable symbolTable;
+
+    public SemanticAnalyzer() {
+        this.symbolTable = new SymbolTable();
+    }
+
+    public SymbolTable getSymbolTable() {
+        return symbolTable;
+    }
+
+    public void declareVariable(String name, String type) throws SemanticException {
+        symbolTable.addSymbol(name, type);
+    }
+
+    public String checkVariable(String name) throws SemanticException {
+        Symbol sym = symbolTable.getSymbol(name);
+        return sym.getType();
+    }
+
+    public String checkOperation(String typeLeft, String typeRight, TokenType operator) throws SemanticException {
+        if (typeLeft.equals("int") && typeRight.equals("int")) {
+            return "int";
+        }
+        
+        if (typeLeft.equals("String") || typeRight.equals("String")) {
+            if (operator == TokenType.PLUS) {
+                return "String";
+            }
+            throw new SemanticException("Error semantico: Operador " + operator + " no soportado para String.");
+        }
+
+        throw new SemanticException("Error semantico: Tipos incompatibles (" + typeLeft + " y " + typeRight + ").");
+    }
+
+    public void checkAssignment(String targetType, String expressionType) throws SemanticException {
+        if (!targetType.equals(expressionType)) {
+            throw new SemanticException("Error semantico: No se puede asignar '" + expressionType + "' a variable de tipo '" + targetType + "'.");
+        }
+    }
+}
