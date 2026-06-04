1cf6357c6ff165ae22215006704cca9d35500395
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-06-04 12:12:37 -0400

    Agrega la clase tokenType y token

---
+++ AnalizadorSintactico/src/Token.java
@@ -0,0 +1,22 @@
+public class Token {
+    private TokenType Tipo;
+    private String Lexema;
+
+    public Token(TokenType tipo, String lexema) {
+        this.Tipo = tipo;
+        Lexema = lexema;
+    }
+
+    public TokenType getType() {
+        return Tipo;
+    }
+
+    public String getLexema() {
+        return Lexema;
+    }
+
+    @Override
+    public String toString() {
+        return Tipo + " -> " + Lexema;
+    }
+}
