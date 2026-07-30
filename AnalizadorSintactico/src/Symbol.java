c5231243beaf292f50eb844ac79974e57542d8d1
juancarlosfermin <juancarlosfermin80@gmail.com>
2026-07-30 16:14:34 -0400

    Nuevos cambios
    Nuevos cambios

--- AnalizadorSintactico/src/Symbol.java
+++ AnalizadorSintactico/src/Symbol.java
@@ -1,10 +1,14 @@
 public class Symbol {
     private String name;
     private String type;
+    
+    // Nuevo campo para soportar valores en ejecución dinámicos (Object) - JuanC
+    private Object valor;
 
     public Symbol(String name, String type) {
         this.name = name;
         this.type = type;
+        this.valor = null; // Inicializado en null por defecto - JuanC
     }
 
     public String getName() {
@@ -13,5 +17,15 @@ public class Symbol {
 
     public String getType() {
         return type;
+    }
+
+    // Getter para obtener el valor dinámico en tiempo de ejecución - JuanC
+    public Object getValor() {
+        return valor;
+    }
+
+    // Setter para actualizar el valor dinámico en tiempo de ejecución - JuanC
+    public void setValor(Object valor) {
+        this.valor = valor;
     }
 }
